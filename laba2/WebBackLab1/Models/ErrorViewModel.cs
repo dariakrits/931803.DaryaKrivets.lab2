895a9f937c475bb6c50c3aa36ec20fa3c242c75c
dariakrits <72679493+dariakrits@users.noreply.github.com>
2021-06-01 16:52:40 +0700

    Add files via upload

---
+++ laba2/WebBackLab1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kosachev.ivan._931701.backlab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
